d185dbec9f617dcaa5da9b9133613ac5f6c875d4
Bernard McKeever <dregin@gmail.com>
2014-01-19 12:50:35 +0000

    Only post user events to the IRC channel... the other's are not craic... most of the time.

--- serf-handler.sh
+++ serf-handler.sh
@@ -3,8 +3,10 @@
 irc_in="/var/tmp/irc/irc.freenode.net/\#serf-test/in"
 if [ -f `${irc_in}` ];
 then
-	echo "New event: ${SERF_EVENT}. Data follows..." > /var/tmp/irc/irc.freenode.net/\#serf-test/in
-	while read line; do
-		echo ${line} > /var/tmp/irc/irc.freenode.net/\#serf-test/in
-	done
+#	echo "New event: ${SERF_EVENT}. Data follows..." > /var/tmp/irc/irc.freenode.net/\#serf-test/in
+	if [ "${SERF_EVENT}" == "user" ]; then
+		while read line; do
+			echo ${line} > /var/tmp/irc/irc.freenode.net/\#serf-test/in
+		done
+	fi
 fi
