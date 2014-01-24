7c193e219ea05b2391a077a9bc3592247feecd1e
Bernard McKeever <dregin@gmail.com>
2014-01-24 13:17:05 +0000

    Write serf handler messages to channel for debug

--- serf-handler.sh
+++ serf-handler.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # event caught - check it's type and stick message in FIFO, if it's the wee-serf
+irc_in=/var/tmp/irc/irc.freenode.net/#serf-test/in
 echo
-echo "New event: ${SERF_EVENT}. Data follows..."
+echo "New event: ${SERF_EVENT}. Data follows..." > ${irc_in}
 while read line; do
-	printf "${line}\n"
+	echo "${line}\n" > ${irc_in}
 done
